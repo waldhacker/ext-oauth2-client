5caa46910026d460ca3d985045d919748ca35f1a
waldhacker-joerg <92523446+waldhacker-joerg@users.noreply.github.com>
2023-03-04 22:27:36 +0100

    [BUGFIX] Load dependencies in non composer mode #6 #14 (#16)
    * [BUGFIX] The missing classes are now included via the ext_emconf.php
    autoloader configuration. #6  #14
    
    * [TASK] TYPO3 v10 non composer compatibility
    
    ---------
    
    Co-authored-by: Ralf Zimmermann <rz@tritum.de>

---
+++ build/release/ext_emconf.php
@@ -0,0 +1,32 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title'            => 'OAuth2 Client',
+    'description'      => 'TYPO3 OAuth2 Login Client (backend and frontend)',
+    'category'         => 'auth',
+    'author'           => 'waldhacker',
+    'author_email'     => 'hello@waldhacker.dev',
+    'author_company'   => 'waldhacker UG (haftungsbeschränkt)',
+    'state'            => 'stable',
+    'uploadfolder'     => '0',
+    'clearCacheOnLoad' => 1,
+    'version'          => '2.0.1',
+    'constraints'      => [
+        'depends' => [
+            'backend' => '10.4.0-11.5.99',
+            'fluid' => '10.4.0-11.5.99',
+            'setup' => '10.4.0-11.5.99',
+            'typo3' => '10.4.0-11.5.99',
+            'php' => '7.4.0-8.1.99',
+        ]
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'Waldhacker\\Oauth2Client\\' => 'Classes',
+        ],
+        'classmap' => [
+            'Resources/Private/PHP/league',
+            'Resources/Private/PHP/paragonie',
+        ],
+    ]
+];
